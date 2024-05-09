65f6d64cf5764ac515737f5038ce278dc8cb9e74
Nitish Jaiswal <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-05-09 12:43:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function removeColor() {
+	var select= document.getElementById("colorSelect");
+	var selectedOption=select.options[select.selectedIndex];
+	if(selectedOption){
+		select.removeChild(selectedOption);
+	}
+}
